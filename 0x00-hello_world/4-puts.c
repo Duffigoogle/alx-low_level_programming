d235f560e748e4d52c34dab5811c7369e72d310f
duffigoogle <calebdduff@gmail.com>
2022-03-10 20:05:46 -0800

    Fixes scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *  main - Entry point
  *  Description: prints "\"Programming is like building a multilingual puzzle"
  *  Return: 0
- */
+*/
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,6 @@
  *  
  * Return: Always 0 (success)
 */
-
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
