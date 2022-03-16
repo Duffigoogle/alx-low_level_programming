65fb0e7473ae36d4b91256613d0ee43d7f455b3d
duffigoogle <calebdduff@gmail.com>
2022-03-15 20:06:23 -0700

    added project files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - This Function prints _putchar using the local header file
+ *  
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char x[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	unsigned int i;
+
+	for (i = 0; i < sizeof(x); i++)
+	{
+		_putchar(x[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
