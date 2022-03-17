50a09208585c0859e1cd34110252de88ae9cdcee
duffigoogle <calebdduff@gmail.com>
2022-03-17 06:35:54 -0700

    Adds Function to 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ * let the numbers speak for themselves
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
