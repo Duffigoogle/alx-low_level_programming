97d4b9e67aca1675a153f72d7365ff381affaacb
duffigoogle <calebdduff@gmail.com>
2022-03-21 09:38:59 -0700

    Fixes compilation errors

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * *_stropy - copies the string pointed to by src, including \0
+ * *_strcpy - copies the string pointed to by src, including \0
  * @src: pointer
  * @dest: pointer
  * Return: the pointer to dest
  */
 
-char *_stropy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i, length;
 
