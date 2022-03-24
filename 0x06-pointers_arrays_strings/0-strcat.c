17db69c6b74cdb9ebed3e52988f817cec768c3ec
duffigoogle <calebdduff@gmail.com>
2022-03-24 03:07:41 -0700

    Betty coding style error fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,26 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int a = -1, i;
+	int count = 0, count2 = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		do {
-			a++;
-			dest[i] = src[a];
-			i++;
-		} while (src[a] != '\0');
-
-		return (dest);
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,27 @@
 #include "main.h"
 /**
- * *_strncat - concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @n: number of bytes
- * Return: void
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
+	int count = 0, count2 = 0;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-		for (i = 0; i < n && src[i] != '\0'; i++)
-			dest[dest_len + i] = src[i];
-	/*should end with a end of string char*/
-	dest[dest_len + i] = '\0';
-
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
