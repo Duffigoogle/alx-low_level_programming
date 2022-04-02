b861268d196cfabcbdcee872b84da84b3101e6fd
duffigoogle <calebdduff@gmail.com>
2022-04-02 12:21:45 -0700

    Creates the static library libmy.a containing some functions

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main-This Function prints _putchar using the local header file
- * Description: Writes a program that prints _putchar, followed by a new line.
+ * main - prints Putchar
+ * Description: prints _putchar
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	char x[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	unsigned int i;
-
-	for (i = 0; i < sizeof(x); i++)
-	{
-		_putchar(x[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
