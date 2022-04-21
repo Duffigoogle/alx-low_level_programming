b9db25d7fcd5a0c65ddfa0f9a961aa0dbedf9847
duffigoogle <calebdduff@gmail.com>
2022-04-21 19:08:52 +0000

    update the lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef SINGLY_LINKED_LISTS
 #define SINGLY_LINKED_LISTS
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -22,4 +27,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* SINGLY_LINKED_LISTS */
