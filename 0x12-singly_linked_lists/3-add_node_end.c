535b502a7f5144e028d3f5be2c342a9b6dabd757
duffigoogle <calebdduff@gmail.com>
2022-04-21 21:49:14 +0000

    Fix error for Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,52 +3,62 @@
 #include <stdio.h>
 #include "lists.h"
 
+
 /**
- * add_node_end - for managment of a list
- * @head: poniter initial
- * @str: new string
- * Return: pointer.
+ * _strlen - returns the length of a string
+ * @m: m variable is a pointer to a char type
+ * Return: Always 0
+ */
+
+int _strlen(const char *m)
+{
+	int len = 0;
+
+	if (m == NULL)
+		return (0);
+
+	while (m[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: poniter initial (to head of list)
+ * @str: new string to be added to node
+ * Return: pointer, or NULL for failure
 */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int stringlen = 0;
-	list_t *new_node = (list_t *) malloc(sizeof(list_t));
-	list_t *last = *head;
+	list_t *new_node;
+	/*a new pointer last, which position we will advance to the last node*/
+	list_t *last;
 
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	if (head == NULL)
-		return (NULL);
-
-	if (str == NULL)
-		return (NULL);
-
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
 
-	if (str != NULL)
-		new_node->str = strdup(str);
-
-	while (new_node->str[stringlen] != '\0')
-		stringlen++;
-
-	new_node->len = stringlen;
-	new_node->next = NULL;
-
+	/*check for if my linked list is empty*/
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (NULL);
+		new_node->next = NULL;
 	}
-
-	while (last->next != NULL)
-		last = last->next;
-	last->next = new_node;
+	else
+	{
+		last = *head;
+		/*go through the chain of nodes to the last node*/
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		last->next = new_node;
+		new_node->next = NULL;
+	}
 
 	return (new_node);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 
 /**
@@ -21,6 +22,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
