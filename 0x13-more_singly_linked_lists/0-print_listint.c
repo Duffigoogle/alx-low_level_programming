ce86c3d166beb0a470d6807004d936c00287bc65
duffigoogle <calebdduff@gmail.com>
2022-04-26 20:34:34 +0000

    Add Task 0 - Print List files

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+  * print_listint - prints all elements of a listint_t list.
+  * @h: pointer to first element in a list.
+  *
+  * Return:  number of nodes.
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	/* define len_list */
+	int len_list;
+
+	/* initialize the len_list */
+	len_list = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		len_list++;
+	}
+
+	return (len_list);
+}
